7e85d13f5c62571093bf090c5f2424fc13a4c59a
BarbuAndreiIulian <83908356+BarbuAndreiIulian@users.noreply.github.com>
2022-02-19 20:03:16 +0100

    Tenzies-ex
    Tenzies-ex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+import React from "react";
+import Die from "./Die";
+
+export default function App() {
+  /* const [dice, setDice] = React.useState(allNewDice());
+   1.Set state.*/
+  /* function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+   2.Create function that returns a new array of random numbers 
+   and set default State to it.*/
+  /*const diceElements = dice.map((die) => <Die value={die} />);
+  3.Map the array you created and create a Dice element with 
+   with inner value of the corespondent map value.*/
+  /*return (
+    <main>
+      <div className="dice-container">{diceElements}</div>
+      
+    </main>
+  );
+   4.Return The Dice Elements.*/
+
+  const [dice, setDice] = React.useState(allNewDice());
+  function allNewDice() {
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      newDice.push(Math.ceil(Math.random() * 6));
+    }
+    return newDice;
+  }
+
+  let diceElements = dice.map((die) => <Die value={die} />);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="dice-container">{diceElements}</div>
+    </main>
   );
 }
-
-export default App;
+/* 
+   1.Set state.
+   2.Create function that returns a new array of random numbers 
+   and set default State to it.
+   3.Map the array you created and create a Dice element with 
+   with inner value of the corespondent map value
+   4.Return The Dice Elements.
+*/
